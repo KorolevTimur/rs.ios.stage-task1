a7f30f7e25743f673548c249532d15ee47f86564
Timur Korolev <timurkorolev89@gmail.com>
2021-05-09 17:00:54 +0300

    feat: solving the fourth task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,15 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSString *string = [number stringValue];
+    if ([string characterAtIndex:0] == '-') {
+        string = [string substringWithRange:NSMakeRange(1, string.length - 1)];
+    }
+    NSMutableArray *array = [NSMutableArray new];
+    for (int i = 0; i < string.length; i++) {
+        [array addObject:[NSString stringWithFormat:@"%C", [string characterAtIndex:i]]];
+    }
+    return [[array reverseObjectEnumerator] allObjects];
 }
 
 @end
