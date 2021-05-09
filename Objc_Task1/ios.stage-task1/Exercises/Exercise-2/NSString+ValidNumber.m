93355c2db9c756e19005c052b82712fb4c4a38e3
Timur Korolev <timurkorolev89@gmail.com>
2021-05-09 14:08:46 +0300

    feat: solving the second task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,14 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    NSLog(@"%@", self);
+    if (self.length >= 0 && self.length <= 100) {
+        NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+        NSNumber *number = [formatter numberFromString:self];
+        return number;
+    } else {
+        return false;
+    }
 }
 
 @end
