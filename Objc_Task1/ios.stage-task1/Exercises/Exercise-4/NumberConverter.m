382b0f478f01d1e36c7aaff51e1da05dcc86ceb9
Timur Korolev <timurkorolev89@gmail.com>
2021-05-10 14:12:05 +0300

    feat: solving the fifth task

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -4,10 +4,11 @@
 
 - (NSArray *)numberConverter:(NSNumber *)number {
     NSString *string = [number stringValue];
+    NSMutableArray *array = [NSMutableArray new];
+    
     if ([string characterAtIndex:0] == '-') {
         string = [string substringWithRange:NSMakeRange(1, string.length - 1)];
     }
-    NSMutableArray *array = [NSMutableArray new];
     for (int i = 0; i < string.length; i++) {
         [array addObject:[NSString stringWithFormat:@"%C", [string characterAtIndex:i]]];
     }
--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,32 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    NSMutableArray *resultArray = [NSMutableArray new];
+    
+    if (playerArray.count == 0) {
+        return resultArray;
+    }
+    else if (rankedArray.count == 0) {
+        for (NSNumber *item in playerArray) {
+            [resultArray addObject: @(1)];
+        }
+        return resultArray;
+    }
+    else {
+        NSOrderedSet *orderedSet = [NSOrderedSet orderedSetWithArray:rankedArray];
+        NSArray *arrayWithoutDuplicates = [orderedSet array];
+    for (int i = 0; i < playerArray.count; i++) {
+        for (int j = 0; j < arrayWithoutDuplicates.count; j++) {
+            if ([playerArray[i] compare: arrayWithoutDuplicates[j]] == NSOrderedSame || [playerArray[i] compare: arrayWithoutDuplicates[j]] == NSOrderedDescending) {
+                [resultArray addObject: [NSNumber numberWithInt: j + 1]];
+                break;
+            } else if (j == arrayWithoutDuplicates.count - 1 && [playerArray[i] compare: arrayWithoutDuplicates[j]] == NSOrderedAscending) {
+                [resultArray addObject: [NSNumber numberWithInt: j + 2]];
+            }
+        }
+    }
+    return resultArray;
+    }
 }
 
 @end
